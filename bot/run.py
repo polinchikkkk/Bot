625212b74959668f89499876865793e4ba5a1827
polinchikkkk <postapekoooo@gmail.com>
2024-09-12 13:30:51 +0300

    Add files via upload

--- bot/run.py
+++ bot/run.py
@@ -25,7 +25,7 @@ async def start(message: Message):
         await message.answer('Start verification')
 
         list_of_files = [file for file in os.listdir('./logs')]
-        # list_of_files.reverse()
+        list_of_files.reverse()
 
         last_open_file = 'fpogjdk'
         
@@ -39,42 +39,28 @@ async def start(message: Message):
 
         path = os.path.join('logs', last_open_file)
 
-        # Имя файла источника
-        list_of_files.reverse()
-        src_file_name = list_of_files[0]
-        path2 = os.path.join('logs', src_file_name)
-
         file = open(path, 'r')
 
         while True:
-
-            with open(path2, 'r') as src_file:
-                src_lines = src_file.readlines()
-
-            for line in src_lines:
-                with open(path, 'a') as dst_file:
-                    dst_file.write(line + '\n')
-
-                time.sleep(1)
             
-                line = file.readline()
+            line = file.readline()
 
-                if not line:
-                    file.close()
-                    time.sleep(300)
-                    break
-
-
-                line = linecache.getline(path, line_for_check)
+            if not line:
+                file.close()
+                time.sleep(300)
+                break
 
 
-                line_for_check, full_message = file_check.err(path, line_for_check)
+            line = linecache.getline(path, line_for_check)
 
-                if full_message:
-                    await send_message(id, full_message)
-                    await asyncio.sleep(1)  
 
-                line_for_check += 1    
+            line_for_check, full_message = file_check.err(path, line_for_check)
+
+            if full_message:
+                await send_message(id, full_message)
+                await asyncio.sleep(1)  
+
+            line_for_check += 1    
 
 
 
