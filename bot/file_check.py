f71323966fd3d48b7ccb2881f165cce732a6a658
polinchikkkk <postapekoooo@gmail.com>
2024-09-19 11:39:56 +0300

    Исправлена проблема с зависанием при отправке /start

--- bot/file_check.py
+++ bot/file_check.py
@@ -28,13 +28,18 @@ def error(session: Session) -> str:
     if flag=='ERROR':
         err_id = id
         err_message = text
-        session.line_for_check += 1
         context = False
         
         #цикл нахождения контекста 
         while True:
-            if re.findall(r'[A-Z]*:  ', linecache.getline(session.last_open_file, session.line_for_check)):
-                id, flag, text = id_flag_text(line=linecache.getline(session.last_open_file, session.line_for_check))
+            session.line_for_check += 1
+            line = linecache.getline(session.last_open_file, session.line_for_check)
+
+            if not line: 
+                return err_message
+
+            if re.findall(r'[A-Z]*:  ', line):
+                id, flag, text = id_flag_text(line=line)
 
                 if id != err_id:
                     # session.line_for_check -= 1 #если нет контекста, мы откатываемся к предыдущей строке, чтобы не потерять строку
@@ -44,20 +49,21 @@ def error(session: Session) -> str:
                 if flag == 'CONTEXT':
                     context = True
                     context_message += text
-                    session.line_for_check += 1
                     break
 
-            session.line_for_check += 1
+
+
 
         #сохранияем текст контекста до тех пор, пока не будет найден новый флаг
         while context:
+            session.line_for_check += 1
+
             line = linecache.getline(session.last_open_file, session.line_for_check)
             #проверка на пустую строку после контекста и на наличие флага в строке
             if not line or re.findall(r'[A-Z]*:  ', line):
                 break
             
             context_message += line
-            session.line_for_check += 1
             
 
         #возвращаем сообщение об ошибке, если она найдена, иначе пустое сообщение
@@ -70,7 +76,6 @@ def error(session: Session) -> str:
 
 #функция обработки ошибки
 def err(session: Session) -> str:
-     
     full_message = error(session) #вызов функции нахождения ошибки
 
     #проверка наличия одинаковых ошибок
@@ -79,10 +84,10 @@ def err(session: Session) -> str:
             session.set_errors.add(full_message)
             return full_message   
 
-        else:
-            session.line_for_check += 1
+    else:
+        session.line_for_check += 1
 
-        return ''
+    return ''
 
 
 
