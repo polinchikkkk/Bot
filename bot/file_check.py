b1803c98693b6b87de6e11d9defd61090b2d13ab
polinchikkkk <postapekoooo@gmail.com>
2024-09-19 11:18:30 +0300

    Add files via upload

--- bot/file_check.py
+++ bot/file_check.py
@@ -29,18 +29,16 @@ def error(session: Session) -> str:
         err_id = id
         err_message = text
         session.line_for_check += 1
-        id, flag, text = id_flag_text(line=linecache.getline(session.last_open_file, session.line_for_check))
-        #проверка на наличие контекста по айди
-
         context = False
         
         #цикл нахождения контекста 
         while True:
-            if id != err_id:
-                return err_message
-
             if re.findall(r'[A-Z]*:  ', linecache.getline(session.last_open_file, session.line_for_check)):
                 id, flag, text = id_flag_text(line=linecache.getline(session.last_open_file, session.line_for_check))
+
+                if id != err_id:
+                    # session.line_for_check -= 1 #если нет контекста, мы откатываемся к предыдущей строке, чтобы не потерять строку
+                    return err_message
 
                 #если нашли флаг контекста, сохранияем текст контекста
                 if flag == 'CONTEXT':
@@ -65,6 +63,7 @@ def error(session: Session) -> str:
         #возвращаем сообщение об ошибке, если она найдена, иначе пустое сообщение
         full_message = err_message + "\n" + context_message
 
+
         return full_message
     else:
         return ''
@@ -75,13 +74,15 @@ def err(session: Session) -> str:
     full_message = error(session) #вызов функции нахождения ошибки
 
     #проверка наличия одинаковых ошибок
-    if full_message in session.set_errors:
-        full_message = ''
-    
-    #проверка пустого сообщения, занесение новой ошибки в сет
     if full_message:
-        session.set_errors.add(full_message)
-        return full_message   
+        if full_message not in session.set_errors:
+            session.set_errors.add(full_message)
+            return full_message   
+
+        else:
+            session.line_for_check += 1
+
+        return ''
 
 
 
--- bot/run.py
+++ bot/run.py
@@ -85,7 +85,7 @@ async def start(message: Message):
                 #если полученная строка пустая, засыпаем и выходим из цикла, ищем снова последний измененный файл
                 if not line:
                     print(f"session.line_for_check: {session.line_for_check}")
-                    await asyncio.sleep(300)
+                    await asyncio.sleep(30)
                     break
                 
                 full_message = file_check.err(session) #получаем сообщение об ошибке 
@@ -95,7 +95,7 @@ async def start(message: Message):
                     await send_message(full_message)
                     await asyncio.sleep(1)  
 
-                session.line_for_check += 1 #итерация строк файла   
+                # session.line_for_check += 1 #итерация строк файла   
     
 async def main():
     await dp.start_polling(bot)
